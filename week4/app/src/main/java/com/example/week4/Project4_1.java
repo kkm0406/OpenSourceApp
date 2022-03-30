af71f5bc735c9e451ad3eacd0842ba8c7818dc30
Kim Kwangmo <sdjmc30412@naver.com>
2022-03-30 13:34:33 +0900

    0330

---
+++ week4/app/src/main/java/com/example/week4/Project4_1.java
@@ -0,0 +1,43 @@
+package com.example.week4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Project4_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText Edit1 = findViewById(R.id.Edit1);
+        EditText Edit2 = findViewById(R.id.Edit2);
+        Button BtnAdd = findViewById(R.id.BtnAdd);
+        Button BtnSub = findViewById(R.id.BtnSub);
+        Button BtnMul = findViewById(R.id.BtnMul);
+        Button BtnDiv = findViewById(R.id.BtnDiv);
+        TextView TextResult = findViewById(R.id.TextResult);
+
+        BtnAdd.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int num1 = Integer.parseInt(Edit1.getText().toString());
+                int num2 = Integer.parseInt(Edit2.getText().toString());
+                TextResult.setText(num1+num2);
+                return false;
+            }
+        });
+
+
+
+
+
+
+    }
+}
